c5161a660618d36ae21209e713ccad7d56968fe0
Arvid Björkström <arvid@bjorkstrom.se>
2014-08-19 13:38:45 +0200

    Move custom theme to app server cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,5 +10,16 @@ include_recipe 'git'
 include_recipe "chef-solo-search"
 include_recipe "users::sysadmins"
 include_recipe "oh-my-zsh"
+search( :users, 'shell:*zsh AND NOT action:remove' ).each do |u|
+  user_id = u["id"]
+
+  template "/home/#{user_id}/.oh-my-zsh/themes/agnoster2.zsh-theme" do
+    source 'agnoster2.zsh-theme.erb'
+    owner 'deploy'
+    group 'deploy'
+    mode '0644'
+    only_if { ::File.directory?("/home/#{user_id}/.oh-my-zsh/themes") }
+  end
+end
 include_recipe 'appserver::dbserver'
 include_recipe 'appserver::webserver'
