f671de5c04f29a248601627529b1c55113eeeabe
Arvid Björkström <arvid@bjorkstrom.se>
2014-08-26 09:47:11 +0200

    Logical mixup

--- recipes/webserver.rb
+++ recipes/webserver.rb
@@ -119,7 +119,7 @@ node['nginx']['sites'].each do |site|
   # Set writeable directories
   if site['writeable_dirs'].kind_of?(Array)
     site['writeable_dirs'].each do |dir_path|
-      dir_path = "#{site['git_path']}/#{dir_path}" if dir_path[0, 1] == '/'
+      dir_path = "#{site['git_path']}/#{dir_path}" unless(dir_path[0, 1] == '/')
       execute "Make #{dir_path} owned by www-data:deploy" do
         command "chown -R www-data:deploy #{dir_path}"
         action :run
