db6c578a332b6aa971ee0d589d35e83a9d7e2167
Arvid Björkström <arvid@bjorkstrom.se>
2015-03-30 16:47:17 +0200

    Set up swap before setting up the services

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,12 +12,12 @@ include_recipe 'users::sysadmins'
 include_recipe 'sudo'
 include_recipe 'oh-my-zsh'
 
-include_recipe 'appserver::userconfig'
-include_recipe 'appserver::dbserver'
-include_recipe 'appserver::webserver'
-
 if node['swapsize'] > 0
   swap_file '/mnt/swap' do
     size node['swapsize']
   end
 end
+
+include_recipe 'appserver::userconfig'
+include_recipe 'appserver::dbserver'
+include_recipe 'appserver::webserver'
