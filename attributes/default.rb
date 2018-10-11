69e64d96193ba616824191fba470e31d48501e8b
Arvid Björkström <arvid@bjorkstrom.se>
2018-10-11 14:59:02 +0200

    AutoMySQL Backup default config

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,10 @@ default['mysql']['server_debian_password'] = 'YouShouldReplaceThis'
 default['mysql']['version'] = '5.7'
 default['mysql']['databases'] = []
 
+# AutoMySQLbackup
 default['automysqlbackup']['mysql_dump_password'] = default['mysql']['server_root_password']
+default['automysqlbackup']['password'] = default['mysql']['server_root_password']
+default['automysqlbackup']['server'] = '127.0.0.1'
 
 # NGINX
 default['nginx']['sites'] = []
